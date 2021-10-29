e02466270dc882c80203d4813620d3504f74a4ae
Ahmet Safa Bülbül <ahmetsafabulbul@gmail.com>
2021-10-29 21:57:01 +0300

    sidebar is added

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import ThemeButton from "../components/ThemeButton";
 import Head from "next/head";
 import LaunchingSoon from "../components/LaunchingSoon";
 import PostLink from "../components/PostLink";
+import PostListLayout from "../components/PostListLayout";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -16,6 +17,33 @@ export async function getStaticProps() {
     },
   };
 }
+
+const categories = [
+  "react",
+  "nextjs",
+  "css",
+  "animasyon",
+];
+
+const SideChildren = ({ children }) => {
+  return (
+    <>
+      <h3 className="text-black">
+        Konu Etiketleri
+      </h3>
+      <div className="grid grid-flow-row grid-cols-3 gap-2">
+        {categories.map(({ item }, index) => (
+          <div
+            key={index}
+            className="bg-gray-50 rounded-sm text-white"
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default function Home({ allPostsData }) {
   return (
@@ -27,34 +55,36 @@ export default function Home({ allPostsData }) {
       </Head>
       <SectionHero />
       <PageBody>
-        <section>
-          <h2 className="font-display text-xl font-bold text-secondary-light antialiased mb-8">
-            {" "}
-            En Son Paylaşılanlar
-          </h2>
-          <ul className="flex flex-col">
-            {allPostsData.map(
-              ({
-                id,
-                date,
-                title,
-                excerpt,
-                category,
-              }) => (
-                <li key={id}>
-                  <PostLink
-                    id={id}
-                    title={title}
-                    date={date}
-                    category={category}
-                  >
-                    {excerpt}
-                  </PostLink>
-                </li>
-              )
-            )}
-          </ul>
-        </section>
+        <PostListLayout>
+          <section>
+            <h2 className="font-display text-xl font-bold text-secondary-light antialiased mb-8">
+              {" "}
+              En Son Paylaşılanlar
+            </h2>
+            <ul className="flex flex-col">
+              {allPostsData.map(
+                ({
+                  id,
+                  date,
+                  title,
+                  excerpt,
+                  category,
+                }) => (
+                  <li key={id}>
+                    <PostLink
+                      id={id}
+                      title={title}
+                      date={date}
+                      category={category}
+                    >
+                      {excerpt}
+                    </PostLink>
+                  </li>
+                )
+              )}
+            </ul>
+          </section>
+        </PostListLayout>
       </PageBody>
       <LaunchingSoon />
     </>
