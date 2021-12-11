148ba53049619c31a1008ae0ccd428d92e9f7478
Ahmet Safa Bülbül <ahmetsafabulbul@gmail.com>
2021-12-11 16:59:21 +0300

    content

--- modules/ServicesList/index.js
+++ modules/ServicesList/index.js
@@ -9,16 +9,10 @@ export default function ServicesList({
   return (
     <ul className={styles.list}>
       <li className={styles.listItem}>
-        <h3>Mobil Uygulama Geliştirme</h3>
+        <h3>Süreç Planlama ve Yönetimi</h3>
         <p>
-          It is a long established fact that a
-          reader will be distracted by the
-          readable content of a page when looking
-          at its layout. The point of using Lorem
-          Ipsum is that it has a more-or-less
-          normal distribution of letters, as
-          opposed to using 'Content here, content
-          here,
+          Süreç içerisinde gerek duyulacak çalışan
+          kaynak vb. ihtiyaçların yönetimi
         </p>
       </li>
       <li className={styles.listItem}>
@@ -35,7 +29,7 @@ export default function ServicesList({
         </p>
       </li>
       <li className={styles.listItem}>
-        <h3>Mobil Uygulama Geliştirme</h3>
+        <h3>Web Geliştirme</h3>
         <p>
           It is a long established fact that a
           reader will be distracted by the
@@ -48,16 +42,32 @@ export default function ServicesList({
         </p>
       </li>
       <li className={styles.listItem}>
-        <h3>Mobil Uygulama Geliştirme</h3>
+        <h3>İşletmenizi İnternete Taşıma</h3>
         <p>
-          It is a long established fact that a
-          reader will be distracted by the
-          readable content of a page when looking
-          at its layout. The point of using Lorem
-          Ipsum is that it has a more-or-less
-          normal distribution of letters, as
-          opposed to using 'Content here, content
-          here,
+          İşletme kimliğinizi ve hizmetlerinizi
+          sizin için internete taşıyabilirim. Web
+          sitesi ile tanınırlığınızı arttırabilir
+          ve müşteri kitlenizi
+          genişletebilirsiniz.
+        </p>
+      </li>
+      <li className={styles.listItem}>
+        <h3>Teknik Destek ve İyileştirmeler</h3>
+        <p>
+          Zaten var olan mobil uygulamalar ve web
+          siteleri için teknik destek
+          sağlayabilir, yeni özellikler
+          kazandırabilir ve bakımlarını
+          yapabilirim.
+        </p>
+      </li>
+      <li className={styles.listItem}>
+        <h3>Ücretsiz Danışmanlık Hizmeti</h3>
+        <p>
+          Yazılım geliştirme süreçleri hakkında
+          fikir almak veya aklınızdaki soruları
+          sormak için ücretsiz olarak randevu
+          alabilirsiniz.
         </p>
       </li>
     </ul>
