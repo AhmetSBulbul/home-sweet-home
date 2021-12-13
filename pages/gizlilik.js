dad54c32db47a3a7c178beb56c36fe39cbb0f111
Ahmet Safa Bülbül <ahmetsafabulbul@gmail.com>
2021-12-13 16:27:11 +0300

    policies

--- pages/gizlilik.js
+++ pages/gizlilik.js
@@ -1,5 +1,184 @@
-import LaunchingSoon from "../components/LaunchingSoon";
+import BaseButton from "../components/BaseButton";
 
 export default function Privacy() {
-  return <LaunchingSoon />;
+  return (
+    <>
+      <section className="content-container prose py-12">
+        <h1>Gizlilik Politikası</h1>
+        <p>Son güncellenme: 13/12/2021</p>
+        <p>
+          Güvenliğiniz bizim için önemli. Bu
+          sebeple bizimle paylaşacağınız kişisel
+          verileriz hassasiyetle korunmaktadır.
+        </p>
+        <p>
+          Biz, Ahmet Safa Bülbül, veri sorumlusu
+          olarak, bu gizlilik ve kişisel verilerin
+          korunması politikası ile, hangi kişisel
+          verilerinizin hangi amaçla işleneceği,
+          işlenen verilerin kimlerle ve neden
+          paylaşılabileceği, veri işleme
+          yöntemimiz ve hukuki sebeplerimiz ile;
+          işlenen verilerinize ilişkin
+          haklarınızın neler olduğu hususunda
+          sizleri aydınlatmayı amaçlıyoruz.
+        </p>
+        <h2>
+          Toplanan Kişisel Verileriniz, Toplanma
+          Yöntemi ve Hukuki Sebebi
+        </h2>
+        <p>
+          IP adresiniz ve kullanıcı aracısı
+          bilgileriniz, sadece analiz yapmak
+          amacıyla ve çerezler (cookies) vb.
+          teknolojiler vasıtasıyla, otomatik veya
+          otomatik olmayan yöntemlerle ve bazen de
+          analitik sağlayıcılar, reklam ağları,
+          arama bilgi sağlayıcıları, teknoloji
+          sağlayıcıları gibi üçüncü taraflardan
+          elde edilerek, kaydedilerek, depolanarak
+          ve güncellenerek, aramızdaki hizmet ve
+          sözleşme ilişkisi çerçevesinde ve
+          süresince, meşru menfaat işleme şartına
+          dayanılarak işlenecektir.
+        </p>
+        <h2>
+          Kişisel Verilerinizin İşlenme Amacı
+        </h2>
+        <p>
+          Bizimle paylaştığınız kişisel
+          verileriniz sadece analiz yapmak
+          suretiyle; sunduğumuz hizmetlerin
+          gerekliliklerini en iyi şekilde yerine
+          getirebilmek, bu hizmetlere sizin
+          tarafınızdan ulaşılabilmesini ve
+          maksiumum düzeyde faydalanılabilmesini
+          sağlamak, hizmetlerimizi, ihtiyaçlarınız
+          doğrultusunda geliştirebilmek ve sizleri
+          daha geniş kapsamlı hizmet sağlayıcıları
+          ile yasal çerçeveler içerisinde
+          buluşturabilmek ve kanundan doğan
+          zorunlulukların (kişisel verilerin talep
+          halinde adli ve idari makamlarla
+          paylaşılması) yerine getirilebilmesi
+          amacıyla, sözleşme ve hizmet süresince,
+          amacına uygun ve ölçülü bir şekilde
+          işlenecek ve güncellenecektir.
+        </p>
+        <h2>
+          Toplanan Kişisel Verilerin Kimlere ve
+          Hangi Amaçlarla Aktarılabileceği
+        </h2>
+        <p>
+          Bizimle paylaştığınız kişisel
+          verileriniz; faaliyetlerimizi yürütmek
+          üzere hizmet aldığımız ve/veya
+          verdiğimiz, sözleşmesel ilişki
+          içerisinde bulunduğumuz,iş birliği
+          yaptığımız, yurt içi ve yurt dışındaki
+          3. şahıslar ile kurum ve kuruluşlara ve
+          talep halinde adli ve idari makamlara,
+          gerekli teknik ve idari önlemler
+          alınması koşulu ile aktarılabilecektir.
+        </p>
+        <h2>
+          Kişisel Verileri İşlenen Kişi Olarak
+          Haklarınız
+        </h2>
+        <p>
+          KVKK madde 11 uyarınca herkes, veri
+          sorumlusuna başvurarak aşağıdaki
+          haklarını kullanabilir:
+        </p>
+        <ol type="a">
+          <li>
+            Kişisel veri işlenip işlenmediğini
+            öğrenme,
+          </li>
+          <li>
+            Kişisel verileri işlenmişse buna
+            ilişkin bilgi talep etme,
+          </li>
+          <li>
+            Kişisel verilerin işlenme amacını ve
+            bunların amacına uygun kullanılıp
+            kullanılmadığını öğrenme,
+          </li>
+          <li>
+            Yurt içinde veya yurt dışında kişisel
+            verilerin aktarıldığı üçüncü kişileri
+            bilme,
+          </li>
+          <li>
+            Kişisel verilerin eksik veya yanlış
+            işlenmiş olması hâlinde bunların
+            düzeltilmesini isteme,
+          </li>
+          <li>
+            Kişisel verilerin silinmesini veya yok
+            edilmesini isteme,
+          </li>
+          <li>
+            (e) ve (f) bentleri uyarınca yapılan
+            işlemlerin, kişisel verilerin
+            aktarıldığı üçüncü kişilere
+            bildirilmesini isteme,
+          </li>
+          <li>
+            İşlenen verilerin münhasıran otomatik
+            sistemler vasıtasıyla analiz edilmesi
+            suretiyle kişinin kendisi aleyhine bir
+            sonucun ortaya çıkmasına itiraz etme,
+          </li>
+          <li>
+            Kişisel verilerin kanuna aykırı olarak
+            işlenmesi sebebiyle zarara uğraması
+            hâlinde zararın giderilmesini talep
+            etme, haklarına sahiptir.
+          </li>
+        </ol>
+        <p>
+          Yukarıda sayılan haklarınızı kullanmak
+          üzere ahmetsafabulbul@gmail.com
+          üzerinden bizimle iletişime
+          geçebilirsiniz.
+        </p>
+        <h2>İletişim</h2>
+        <p>
+          Sizlere hizmet sunabilmek amaçlı
+          analizler yapabilmek için, sadece
+          gerekli olan kişisel verilerinizin, işbu
+          gizlilik ve kişisel verilerin işlenmesi
+          politikası uyarınca işlenmesini, kabul
+          edip etmemek hususunda tamamen
+          özgürsünüz. Siteyi kullanmaya devam
+          ettiğiniz takdirde kabul etmiş olduğunuz
+          tarafımızca varsayılacak olup, daha
+          ayrıntılı bilgi için bizimle
+          ahmetsafabulbul@gmail.com e-mail adresi
+          üzerinden iletişime geçmekten lütfen
+          çekinmeyiniz.
+        </p>
+      </section>
+      <div className="content-container py-8">
+        <ul className="flex flex-col laptop:flex-row laptop:space-x-4 justify-items-end items-end">
+          <li>
+            <BaseButton href="/gizlilik">
+              Gizlilik Politikası
+            </BaseButton>
+          </li>
+          <li>
+            <BaseButton href="/kullanim-kosullari">
+              Kullanım Koşulları
+            </BaseButton>
+          </li>
+          <li>
+            <BaseButton href="/cerez-politikasi">
+              Çerez Politikası
+            </BaseButton>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
